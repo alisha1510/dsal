23776545722e9bf10640730d11992f4ef38a94f1
Alisha <alishasheikh7512@gmail.com>
2025-06-17 21:43:54 +0530

    prac2.cpp

--- 2-student.cpp
+++ 2-student.cpp
@@ -1,3 +1,4 @@
+//A book consists of chapters, chapters consist of sections and sections consist of subsections. Construct a tree and print the nodes. Find the time and space requirements of your method.
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
