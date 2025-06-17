b834fec0e7729d6de03ea300b5c5f9751519b57a
Alisha <alishasheikh7512@gmail.com>
2025-06-17 21:47:59 +0530

    prac1.cpp

--- 1-book.cpp
+++ 1-book.cpp
@@ -1,4 +1,5 @@
-//A book consists of chapters, chapters consist of sections and sections consist of subsections. Construct a tree and print the nodes. Find the time and space requirements of your method.
+/*A book consists of chapters, chapters consist of sections and sections consist of subsections. 
+Construct a tree and print the nodes. Find the time and space requirements of your method.*/
 
 #include<iostream>
 using namespace std;
